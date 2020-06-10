f3856a1d27a3806c71614cea77512a1cb43224e6
tdye.dev@gmail.com <tdye_77@yahoo.com>
2020-06-09 22:48:37 -0400

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -18,7 +18,7 @@ var connection;
 // });
 
 if (process.env.JAWSDB_URL) {
-connection = mysql.createConnecton(process.env.JAWSDB_URL);
+connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection( {
   host: "localhost",
